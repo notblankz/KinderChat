bd5f9a0e8ad86a140dcaf482fb0c1ebe0b4d20af
blankz <aahan3005@outlook.com>
2024-08-30 19:09:42 +0530

    initial commit

--- api/app.py
+++ api/app.py
@@ -1,1 +1,18 @@
-im
+from flask import Flask
+from transformers import pipeline
+
+app = Flask(__name__)
+
+toxic_bert = pipeline("text-classification", model="unitary/toxic-bert", tokenizer="unitary/toxic-bert", return_all_scores=True)
+
+def analyze_message(message):
+    result = toxic_bert(message)
+
+    print(result)
+
+
+if __name__ == '__main__':
+    message = ""
+    while ( message != "exit"):
+        message - input("Enter a msg: ")
+        analyze_message(message)
